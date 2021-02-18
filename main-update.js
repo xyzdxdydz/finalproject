9193fb9d93f1640ff3f23d5e86eff4df9dc4d71f
Suchon Prasert <58396402+toey10112@users.noreply.github.com>
2021-02-19 02:27:53 +0700

    add fetch

--- main-update.js
+++ main-update.js
@@ -3,10 +3,29 @@ function makeNewNode(text) {
     newNode.innerText = text;
     return newNode;
 }
-function getData(id,water){
-    var data = document.getElementById(id)
-    data.innerHTML = water
+function getPopularData(id,water){
+    let data = document.getElementById(`show-${id}`);
+    data.innerText = water.popularwater
+    // prayut.innerText = water.result + "\n"+ "\n"+ "\n" + water.result
+
+    console.log(water)
 }
+
+function getWater1Data(id,water){
+    let data = document.getElementById(`water1-${id}`);
+        data.innerText = water.water1
+    console.log(water)
+
+}
+
+function getWater2Data(id,water){
+    let data = document.getElementById(`water2-${id}`);
+        data.innerText = water.water2
+    console.log(water)
+
+}
+
+
 
 let cache = [];
 a = document.createElement("h1");
@@ -15,6 +34,8 @@ a.innerText = "Welcome back : Yo";
 i = document.createElement("h3");
 i.innerText = "What's new with your friend? Check something below!"
 
+
+
 setInterval(() => {
     fetch("http://158.108.182.12:3000/popular?Type=hoursPopular", {
         method:"GET",
@@ -22,7 +43,25 @@ setInterval(() => {
 
     })
     .then((data) => data.json())
-    .then(data => console.log("hoursPopular = "+ data.result)).then(getData("hourly",data));
+    .then(data => getPopularData("hourly",data));
+
+    fetch("http://158.108.182.12:3000/popular?Type=hoursPopular", {
+        method:"GET",
+        headers: {"Content-Type": "application/json"},
+
+    })
+    .then((data) => data.json())
+    .then(data => getWater1Data("hourly",data));
+
+    fetch("http://158.108.182.12:3000/popular?Type=hoursPopular", {
+        method:"GET",
+        headers: {"Content-Type": "application/json"},
+
+    })
+    .then((data) => data.json())
+    .then(data => getWater2Data("hourly",data));
+
+
 
     fetch("http://158.108.182.12:3000/popular?Type=dayPopular", {
         method:"GET",
@@ -30,12 +69,43 @@ setInterval(() => {
 
     })
     .then((data) => data.json())
-    .then(data => console.log("dayPopular = "+ data.result)).then(getData("daily",data));
+    .then(data => getPopularData("daily",data));
+
+        fetch("http://158.108.182.12:3000/popular?Type=dayPopular", {
+        method:"GET",
+        headers: {"Content-Type": "application/json"},
+
+    })
+    .then((data) => data.json())
+    .then(data => getWater1Data("daily",data));
+
+            fetch("http://158.108.182.12:3000/popular?Type=dayPopular", {
+        method:"GET",
+        headers: {"Content-Type": "application/json"},
+
+    })
+    .then((data) => data.json())
+    .then(data => getWater2Data("daily",data));
+
+
+
 
     fetch("http://158.108.182.12:3000/popular?Type=weekPopular", {
         method:"GET",
         headers: {"Content-Type": "application/json"},
     })
     .then((data) => data.json())
-    .then(data => console.log("dayPopular = "+ data.result)).then(getData("weekly",data));
+    .then(data => getPopularData("weekly",data));
+     fetch("http://158.108.182.12:3000/popular?Type=weekPopular", {
+        method:"GET",
+        headers: {"Content-Type": "application/json"},
+    })
+    .then((data) => data.json())
+    .then(data => getWater2Data("weekly",data));
+      fetch("http://158.108.182.12:3000/popular?Type=weekPopular", {
+        method:"GET",
+        headers: {"Content-Type": "application/json"},
+    })
+    .then((data) => data.json())
+    .then(data => getWater1Data("weekly",data));
 }, 5000);
