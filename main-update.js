90d4b3a8dc48d0ec79689706b340a64ade32df17
DIMMOY <dimmy6789@gmail.com>
2021-02-18 13:34:35 +0700

    d

--- main-update.js
+++ main-update.js
@@ -12,13 +12,17 @@ i = document.createElement("h3");
 i.innerText = "What's new with your friend? Check something below!"
 
 setInterval(() => {
-    fetch("http://158.108.182.0:20010/app/admin/exceed_group10/g10/602ceb00df6a3e7117266daf")
+    fetch("http://http://158.108.182.0:20010/app/admin/exceed_group10/g10/602ceb00df6a3e7117266daf", {
+        //method:"GET",
+        //headers: {"Content-Type": "application/json"},
+        //body: JSON.stringify ()
+    })
     .then((data) => data.json())
     .then(datas => { 
         datas.forEach(d => {
-            app.appendChild(makeNewNode(d.Slot));
-            app.appendChild(makeContent(d.Volume));
-            app.appendChild(makeNewNode(d.Is_on));
+            app.appendChild(makeNewNode(data.Slot));
+            app.appendChild(makeContent(data.Volume));
+            app.appendChild(makeNewNode(data.Is_on));
         });
     });
 }, 5000);
