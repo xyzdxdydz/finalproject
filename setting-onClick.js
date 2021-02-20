2ebeb2813343dce2210485836a4818d356a14629
xyzdxdydz <mastermind2012@hotmail.co.th>
2021-02-20 20:33:55 +0700

    + about

--- setting-onClick.js
+++ setting-onClick.js
@@ -2,6 +2,10 @@ var btn = document.getElementById("setting");
 var modal = document.getElementById("modal");
 var span = document.getElementsByClassName("close")[0];
 
+var btnA = document.getElementById("button-about");
+var modalA = document.getElementById("modal-about");
+var spanA = document.getElementsByClassName("close")[1];
+
 btn.onclick = function() {
 modal.style.display = "block";
 }
@@ -13,5 +17,19 @@ span.onclick = function() {
 window.onclick = function(event) {
     if (event.target == modal) {
     modal.style.display = "none";
+    }
+}
+
+btnA.onclick = function() {
+    modalA.style.display = "block";
+    }
+    
+spanA.onclick = function() {
+    modalA.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modalA) {
+    modalA.style.display = "none";
     }
 }
